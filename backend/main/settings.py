4bb39e6e3b80bf06e234b7df83d9f9ba1da7773a
unknown <keegood8@gmail.com>
2020-08-23 19:51:39 -0700

    added users app and set auth model in settings

--- backend/main/settings.py
+++ backend/main/settings.py
@@ -23,6 +23,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -104,3 +106,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+AUTH_USER_MODEL = users.User
+
---
+++ backend/users/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path
+
+# from .views import UserList, UserProfile
+from . import views
+urlpatterns = [
+    path('', views.user_list),
+    path('<int:pk>/', views.user_detail),
+    path('new/', views.user_create),
+    path('edit/<int:pk>', views.user_edit),
+]
