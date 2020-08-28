1d5f86cee64e37b65b58485e666a76e56fc7dce3
unknown <keegood8@gmail.com>
2020-08-27 23:07:36 -0700

    added permissions and authentication on appliance list view

--- backend/appliances/views.py
+++ backend/appliances/views.py
@@ -1,13 +1,23 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework.decorators import (
+    api_view, 
+    authentication_classes,
+    permission_classes
+)
+from rest_framework.authentication import (
+    BasicAuthentication,
+    TokenAuthentication
+)
+from rest_framework.permissions import IsAuthenticated
 
 from .models import Appliance
 
-# class ApplianceViewSet(viewsets.ViewSet):
 
 @api_view(['GET'])
+@authentication_classes([BasicAuthentication, TokenAuthentication])
+@permission_classes([IsAuthenticated])
 def appliance_list(request):
     appliances=Appliance.objects.all()
 
