30fda277bbfb2588687181668b7d70e00c68ab80
unknown <keegood8@gmail.com>
2020-09-18 12:41:12 -0700

    save refresh token to http only cookie, refresh access token

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { CookiesProvider } from "react-cookie";
-
 import "../node_modules/bootstrap/dist/css/bootstrap.css";
 import $ from "jquery";
 import Popper from "popper.js";
