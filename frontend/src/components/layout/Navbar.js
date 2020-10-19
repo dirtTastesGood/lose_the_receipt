c4f8bddc0f4dbe118d531b64dee695f09ca1de8e
dirtTastesGood <keegood8@gmail.com>
2020-10-19 15:45:38 -0700

    fixed nav buttons and fonts

--- frontend/src/components/layout/Navbar.js
+++ frontend/src/components/layout/Navbar.js
@@ -1,8 +1,8 @@
-import React, { Fragment, useContext, useEffect } from "react";
-import { Link } from "react-router-dom";
-import "./scss/navbar.scss";
-import { ReactComponent as Logo } from "../../images/logo.svg";
-import AuthContext from "../../context/auth/authContext";
+import React, { Fragment, useContext, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import './scss/navbar.scss';
+import { ReactComponent as Logo } from '../../images/logo.svg';
+import AuthContext from '../../context/auth/authContext';
 
 const Navbar = () => {
   const authContext = useContext(AuthContext);
@@ -22,14 +22,14 @@ const Navbar = () => {
 
   const guestLinks = (
     <Fragment>
-      <li className="nav-item">
-        <Link className="nav-link" to="/register">
-          <h4 className="">Register</h4>
+      <li className='nav-item'>
+        <Link className='nav-link' to='/register'>
+          <h4 className=''>Register</h4>
         </Link>
       </li>
-      <li className="nav-item">
-        <Link className="nav-link" to="/login">
-          <h4 className="">Login</h4>
+      <li className='nav-item'>
+        <Link className='nav-link' to='/login'>
+          <h4 className=''>Login</h4>
         </Link>
       </li>
     </Fragment>
@@ -37,44 +37,42 @@ const Navbar = () => {
 
   const authLinks = (
     <Fragment>
-      <li className="nav-item">
-        <Link className="nav-link" to="/appliances">
-          <h4 className="mr-2">
-            Appliances
-            </h4>
+      <li className='nav-item'>
+        <Link className='nav-link' to='/appliances'>
+          <h4 className='mr-2'>Appliances</h4>
         </Link>
       </li>
-      <li className="nav-item">
-        <Link className="nav-link" to="/account">
-          <h4 className="">Account</h4>
+      <li className='nav-item'>
+        <Link className='nav-link' to='/account'>
+          <h4 className=''>Account</h4>
         </Link>
       </li>
-      <li className="nav-item">
-        <Link className="nav-link" onClick={onLogout}>
-          <h4 className="">Logout</h4>
+      <li className='nav-item'>
+        <Link className='nav-link' onClick={onLogout}>
+          <h4 className=''>Logout</h4>
         </Link>
       </li>
     </Fragment>
   );
 
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-info">
-      <Link className="navbar-brand " to="/">
+    <nav className='navbar navbar-expand-lg navbar-dark bg-info'>
+      <Link className='navbar-brand ' to='/'>
         <Logo />
       </Link>
       <button
-        className="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarNav"
-        aria-controls="navbarNav"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
+        className='navbar-toggler'
+        type='button'
+        data-toggle='collapse'
+        data-target='#navbarNav'
+        aria-controls='navbarNav'
+        aria-expanded='false'
+        aria-label='Toggle navigation'
       >
-        <span className="navbar-toggler-icon"></span>
+        <span className='navbar-toggler-icon'></span>
       </button>
-      <div className="collapse navbar-collapse" id="navbarNav">
-        <ul className="navbar-nav text-center">
+      <div className='collapse navbar-collapse' id='navbarNav'>
+        <ul className='navbar-nav text-center'>
           {isAuthenticated ? authLinks : guestLinks}
         </ul>
       </div>
